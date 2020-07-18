360850782af940a4b8c1d5c6155b4f415780eb29
Ruben <vim.ruben@hotmail.com>
2020-07-18 14:10:39 -0300

    Creación y definición de estilos

---
+++ JS/js.js
@@ -0,0 +1,34 @@
+$(document).ready(function () {
+    var scroll = $(window).scrollTop();
+    $("#posicion").text(scroll);
+    $(window).scroll(function (event) {
+      var scroll = $(window).scrollTop();
+      $("#posicion").text(scroll);
+      /*Cambia el color del div cuando es distinto a 0*/
+      if (scroll != 0) {
+        $("#nav-bar").css({
+          background: "rgba(38, 38, 38, 0.95)",
+        });
+      } else {
+        $("#nav-bar").css({
+          background: "black",
+        });
+      }
+    });
+  });
+
+  $(document).ready(function(){
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
+  });
+
+
+  // (function($){
+  //   $(function(){
+  
+  //     $('.sidenav').sidenav();
+  //     $('.parallax').parallax();
+  
+  //   }); // end of document ready
+  // })(jQuery); // end of jQuery name space
+
